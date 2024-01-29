669254b07570f032a69077e724f4064cbc5f1091
jeisonTrabajo <jw.vera.r@gmail.com>
2024-01-29 16:28:39 -0500

    cambio documento carpeta tres app.js

--- 3-pensamiento-logico/app.js
+++ 3-pensamiento-logico/app.js
@@ -1,7 +1,7 @@
 /* Ejercicio 3: Pensamiento lógico
-Escribe una función que tome un número entero como entrada y devuelva un array con 
-todos los números enteros impares desde 1 hasta el número de entrada. Por ejemplo, si el 
-número de entrada es 9, la función debería devolver [1, 3, 5, 7, 9]. */
+Escribe una función que tome un número entero como entrada y devuelva un array con todos los números enteros impares desde 1 hasta el número de entrada. Por ejemplo, si el 
+número de entrada es 9, la función debería devolver [1, 3, 5, 7, 9].
+ */
 
 const arraysOdd = (number) => {
   if (typeof number !== "number") {
